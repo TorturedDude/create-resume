6c13ebb417dfe9b6becbbf622709affb0d1fdd7d
a.golovanov <a.golovanov@goodt.me>
2025-03-23 04:58:25 -0700

    init project

---
+++ src/main/kotlin/ru/resume/create_resume/CreateResumeApplication.kt
@@ -0,0 +1,11 @@
+package ru.resume.create_resume
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreateResumeApplication
+
+fun main(args: Array<String>) {
+	runApplication<CreateResumeApplication>(*args)
+}
---
+++ src/test/kotlin/ru/resume/create_resume/CreateResumeApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.resume.create_resume
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreateResumeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
